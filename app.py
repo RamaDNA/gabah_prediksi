5da51ac9b7ee35578cf3a98386d3aea37e640532
nofian bismika ariyatma <rama99ever@gmail.com>
2024-08-23 22:58:52 +0700

    test

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ output_details = interpreter.get_output_details()
 
 # Define function to make predictions
 def predict(image: Image.Image):
-    img = image.resize((600, 600))  # Resize to the model's expected size
+    img = image.resize((224, 224))  # Resize to the model's expected size
     img_array = np.array(img).astype(np.float32) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
 
